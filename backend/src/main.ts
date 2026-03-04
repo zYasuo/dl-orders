fcc91100fec272543348d1b890ed48ba1f7428bb
zYasuo <dansilvaagr@hotmail.com>
2026-03-03 22:05:50 -0300

    refactor: adjust indentation in main.ts for improved readability
    - Standardized indentation in the main.ts file to enhance code clarity and maintain consistency across the codebase.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 import { rabbitmqOptions } from './config/rabbitMQ/rabbitmq.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.connectMicroservice(rabbitmqOptions);
-  await app.startAllMicroservices();
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.connectMicroservice(rabbitmqOptions);
+    await app.startAllMicroservices();
+    await app.listen(3000);
 }
 bootstrap();
